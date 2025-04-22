de9cec98ce0edce0f7753fe89e706e519c4229ce
the_voices <adamashrafsvs@gmail.com>
2025-04-22 19:22:10 +0200

    created admin model, updated user model

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -5,9 +5,8 @@ const userSchema = new mongoose.Schema({
     Name: { type: String, required: true },
     Email: { type: String, required: true, unique: true },
     Password: { type: String, required: true },
-    Phone: { type: String, required: true },
-    Address: { type: String, required: true },
+    
 });
 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('admin', adminSchema);
 module.exports.Schema = userSchema; 
